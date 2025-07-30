ba88f854961ed31ebd9ab959b9629fd670ab6204
DulguunMunkhzol <97315718+DulguunMunkhzol@users.noreply.github.com>
2025-07-30 16:02:12 -0400

    first commit

---
+++ src/test/java/com/practice/MangaCollection/MangaCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.MangaCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
